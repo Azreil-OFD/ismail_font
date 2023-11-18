18114f726ae5c5f627e7775104e1e7f485e7aae8
Azreil-OFD <fantom2413@gmail.com>
2023-11-19 00:00:45 +0500

    add build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import mkcert from "vite-plugin-mkcert";
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/ismail_font/",
     plugins: [vue()],
     resolve: {
         alias: {
